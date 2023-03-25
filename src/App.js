9f788439840e3379df88799335f048ef3366e7c8
DearestAtivie <dear.ativie@gmail.com>
2023-03-25 10:40:20 -0500

    imported components into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from '.components/Title.js'
+import TodayIs from '.components/TodayIs.js'
+import Dropdown from '.components/Dropdown.js'
+import On from '.components/On.js'
+import Dropdown2 from '.components/Dropdown2.js'
+import IamFeeling from '.components/IamFeeling.js'
+import Emojis from '.components/Emojis.js'
 
 function App() {
   return (
