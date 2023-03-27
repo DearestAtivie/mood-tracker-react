b6d70162651cf90516d8815c0c7821a395f06fc6
DearestAtivie <dear.ativie@gmail.com>
2023-03-26 22:52:15 -0500

    added coding for two dropdowns - no functionality yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Title from '.components/Title.js'
-import Description from './components/Description';
-import TodayIs from '.components/TodayIs.js'
-import Dropdown from '.components/Dropdown.js'
-import On from '.components/On.js'
-import Dropdown2 from '.components/Dropdown2.js'
-import IamFeeling from '.components/IamFeeling.js'
-import Emojis from '.components/Emojis.js'
+import Title from './components/Title.js'
+import Description from './components/Description.js';
+import TodayIs from './components/TodayIs.js'
+import Dropdown1 from './components/Dropdown1.js'
+import On from './components/On.js'
+import Dropdown2 from './components/Dropdown2.js';
+//import Dropdown2 from './components/Dropdown2.js'
+//import IamFeeling from './components/IamFeeling.js'
+//import Emojis from './components/Emojis.js'
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
        <Title HeaderContent="Know Thyself"/>
        <Description MessageContent="A mood tracking app designed to help you better recognize your emotions"/>
        <TodayIs MessageContent="Today Is"/>
+       <Dropdown1/>
        <On MessageContent="On"/>
+       <Dropdown2/>
 
     </div>
   );
--- src/components/Dropdown2.js
+++ src/components/Dropdown2.js
@@ -1,3 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Dropdown2 = (props) => {
+
+    return(
+        <>
+        <EntireDD2Styling>
+        <DD2Container>
+            <DD2Header>Time of Day</DD2Header>
+            <DD2ListContainer>
+                <DD2List>
+                    <DD2ListItem>Morning</DD2ListItem>
+                    <DD2ListItem>Afternoon</DD2ListItem>
+                    <DD2ListItem>Evening</DD2ListItem>
+                </DD2List>
+            </DD2ListContainer>
+        </DD2Container>
+        </EntireDD2Styling>   
+                </>
+
+    );
+}
 
 export default Dropdown2
+
+const EntireDD2Styling = styled.div`
+font-family: sans-serif;
+background: #f0f0f0;
+height: 100vh;
+`;
+
+
+const DD2Container = styled.div`
+width: 10.5em;
+  margin: 0 auto;
+`;
+
+const DD2Header = styled.div`
+margin-bottom: 0.8em;
+  padding: 0.4em 2em 0.4em 1em;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  font-weight: 500;
+  font-size: 1.3rem;
+  color: #3faffa;
+  background: #ffffff;
+`;
+
+const DD2ListContainer = styled.div`
+`;
+
+const DD2List = styled.ul`
+padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: #3faffa;
+  font-size: 1.3rem;
+  font-weight: 500;
+  &:first-child {
+    padding-top: 0.8em;
+`;
+
+const DD2ListItem = styled.li`
+list-style: none;
+  margin-bottom: 0.8em;
+`;
